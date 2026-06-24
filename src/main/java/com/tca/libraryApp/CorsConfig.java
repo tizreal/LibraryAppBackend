548adaf961ea1768b6c42ec62f399912278f50b3
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2026-06-24 01:24:33 -0400

    same fix

--- src/main/java/com/tca/libraryApp/CorsConfig.java
+++ src/main/java/com/tca/libraryApp/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "http://localhost:5173",
-                    "https://library-app-hazel-psi.vercel.app"
+                    "https://*.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
