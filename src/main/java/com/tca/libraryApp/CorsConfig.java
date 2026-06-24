269497ab504ae53ed103849eda4c8d41341cfd5d
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2026-06-24 01:28:41 -0400

    allowedOriginPatterns fix

--- src/main/java/com/tca/libraryApp/CorsConfig.java
+++ src/main/java/com/tca/libraryApp/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                     "http://localhost:5173",
                     "https://*.vercel.app"
                 )
